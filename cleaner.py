c7ca746697bf689dff8ae24be2818147b7891ba1
hepnerb <hepnerbryce@gmail.com>
2021-12-09 17:56:31 -0700

    moved things around

--- cleaner.py
+++ cleaner.py
@@ -2,9 +2,8 @@ import numpy as np
 import pandas as pd
 
 okdata = pd.read_excel('CleanedDL.xlsx')
-okdata = okdata.iloc[:,0:]
-branddummies = pd.get_dummies(okdata.iloc[:,2], drop_first=True)
+okdata = okdata.iloc[:,1:]
+branddummies = pd.get_dummies(okdata.iloc[:,1], drop_first=True)
 okdata = pd.concat([okdata, branddummies], axis=1)
 okdata = okdata.drop(columns=['Brand'])
-print(okdata.iloc[:,1:])
 okdata.to_csv('CleanedDL.csv')
