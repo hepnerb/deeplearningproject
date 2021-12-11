30a37d54aa3e9d2f814ea99ace2c2f9d4f982143
hepnerb <hepnerbryce@gmail.com>
2021-12-10 21:29:08 -0700

    I hope no one sees these

--- cleaner.py
+++ cleaner.py
@@ -6,4 +6,7 @@ okdata = okdata.iloc[:,1:]
 branddummies = pd.get_dummies(okdata.iloc[:,1], drop_first=True)
 okdata = pd.concat([okdata, branddummies], axis=1)
 okdata = okdata.drop(columns=['Brand'])
-okdata.to_csv('CleanedDL.csv')
+test = pd.concat([okdata.iloc[10:11],okdata.iloc[16:17],okdata.iloc[20:21],okdata.iloc[25:26],okdata.iloc[45:46]])
+okdata = okdata.drop([10,16,20,25,45], axis=0)
+test.to_csv('test_data.csv')
+okdata.to_csv('train_data.csv')
